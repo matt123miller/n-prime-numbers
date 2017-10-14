6f78e6b30c566e5b9a700aa77d98e945f364c2ad
Matt Mac <mmiller@tfspark.com>
2017-10-14 19:09:38 +0100

    Boom, the test passes given a value of one. This is obviously going to be tedious but I discovered the [Theory] attribute to run a test multiple times with different values. I'll add that in next.

--- PrimesApp/Primes.cs
+++ PrimesApp/Primes.cs
@@ -6,7 +6,10 @@ namespace PrimesApp
     {
         public bool IsPrime(int candidate) 
         {
-            
+            if (candidate == 1)
+            {
+                return false;
+            }
             throw new NotImplementedException("Please create a test first");
         }
     }
